33cf8a2cd699683ed56b187c6a175386d589fab3
PavloSliepchenko <115042054+PavloSliepchenko@users.noreply.github.com>
2023-10-25 15:31:01 -0500

    Added book entity, repository and service to it (#1)
    * Added book entity, repository and service to it
    
    * Replaced annotation @Nonnull with @Column(nullable = false)

---
+++ src/main/java/com/example/onlinebookstore/repository/impl/BookRepositoryImpl.java
@@ -0,0 +1,57 @@
+package com.example.onlinebookstore.repository.impl;
+
+import com.example.onlinebookstore.model.Book;
+import com.example.onlinebookstore.repository.BookRepository;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public BookRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public Book save(Book book) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.persist(book);
+            transaction.commit();
+            return book;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Cannot add a book " + book, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Book> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            CriteriaQuery<Book> query = builder.createQuery(Book.class);
+            Root<Book> root = query.from(Book.class);
+            return session.createQuery(query).getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Cannot get all books", e);
+        }
+    }
+}
