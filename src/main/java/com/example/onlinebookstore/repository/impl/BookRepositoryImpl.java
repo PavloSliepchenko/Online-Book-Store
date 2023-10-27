9507fdd6da7e7efab74567d0f5cf677a3cc0b521
PavloSliepchenko <sliepchenkopavlo@gmail.com>
2023-10-27 13:02:23 -0500

    Created and implemented DTOs, added Mapstruct and mappers, created BookController, added custom exception

--- src/main/java/com/example/onlinebookstore/repository/impl/BookRepositoryImpl.java
+++ src/main/java/com/example/onlinebookstore/repository/impl/BookRepositoryImpl.java
@@ -1,57 +1,55 @@
 package com.example.onlinebookstore.repository.impl;
 
+import com.example.onlinebookstore.exception.EntityNotFoundException;
 import com.example.onlinebookstore.model.Book;
 import com.example.onlinebookstore.repository.BookRepository;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Root;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import java.util.List;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class BookRepositoryImpl implements BookRepository {
-    private final SessionFactory sessionFactory;
-
-    @Autowired
-    public BookRepositoryImpl(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
+    private final EntityManagerFactory entityManagerFactory;
 
     @Override
     public Book save(Book book) {
-        Session session = null;
-        Transaction transaction = null;
-        try {
-            session = sessionFactory.openSession();
-            transaction = session.beginTransaction();
-            session.persist(book);
+        EntityTransaction transaction = null;
+        try (EntityManager manager = entityManagerFactory.createEntityManager()) {
+            transaction = manager.getTransaction();
+            transaction.begin();
+            manager.persist(book);
             transaction.commit();
             return book;
         } catch (Exception e) {
-            if (transaction != null) {
+            if (transaction != null && transaction.isActive()) {
                 transaction.rollback();
             }
-            throw new RuntimeException("Cannot add a book " + book, e);
-        } finally {
-            if (session != null) {
-                session.close();
-            }
+            throw new EntityNotFoundException("Cannot add a book " + book, e);
         }
     }
 
     @Override
     public List<Book> findAll() {
-        try (Session session = sessionFactory.openSession()) {
-            CriteriaBuilder builder = session.getCriteriaBuilder();
-            CriteriaQuery<Book> query = builder.createQuery(Book.class);
-            Root<Book> root = query.from(Book.class);
-            return session.createQuery(query).getResultList();
+        try (EntityManager manager = entityManagerFactory.createEntityManager()) {
+            return manager.createQuery("SELECT b FROM Book b", Book.class)
+                    .getResultList();
         } catch (Exception e) {
-            throw new RuntimeException("Cannot get all books", e);
+            throw new EntityNotFoundException("Cannot get all books", e);
+        }
+    }
+
+    @Override
+    public Optional<Book> findBookById(Long id) {
+        try (EntityManager manager = entityManagerFactory.createEntityManager()) {
+            Book book = manager.find(Book.class, id);
+            return Optional.ofNullable(book);
+        } catch (Exception e) {
+            throw new EntityNotFoundException("Cannot find a book by id " + id, e);
         }
     }
 }
