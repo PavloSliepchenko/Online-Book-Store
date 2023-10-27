83e4f6efcbbc9a7f523d1177ec8e7b59a0f077fb
PavloSliepchenko <sliepchenkopavlo@gmail.com>
2023-10-27 11:19:50 -0500

    Created and implemented DTOs, controller, Mapstruct and mappers, custom exception

--- src/main/java/com/example/onlinebookstore/OnlineBookStoreApplication.java
+++ src/main/java/com/example/onlinebookstore/OnlineBookStoreApplication.java
@@ -1,35 +1,11 @@
 package com.example.onlinebookstore;
 
-import com.example.onlinebookstore.model.Book;
-import com.example.onlinebookstore.service.BookService;
-import java.math.BigDecimal;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OnlineBookStoreApplication {
-    @Autowired
-    private BookService bookService;
-
     public static void main(String[] args) {
         SpringApplication.run(OnlineBookStoreApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner commandLineRunner() {
-        return args -> {
-            Book book = new Book();
-            book.setTitle("Round up 5");
-            book.setIsbn("9781405888813");
-            book.setPrice(BigDecimal.valueOf(405));
-            book.setDescription("The updated Grammar Textbook Round-Up with "
-                    + "CD-Rom is a great addition to all school textbooks.");
-            bookService.save(book);
-
-            System.out.println(bookService.findAll());
-        };
     }
 }
--- src/main/java/com/example/onlinebookstore/service/impl/BookServiceImpl.java
+++ src/main/java/com/example/onlinebookstore/service/impl/BookServiceImpl.java
@@ -1,24 +1,39 @@
 package com.example.onlinebookstore.service.impl;
 
+import com.example.onlinebookstore.dto.BookDto;
+import com.example.onlinebookstore.dto.CreateBookRequestDto;
+import com.example.onlinebookstore.exception.EntityNotFoundException;
+import com.example.onlinebookstore.mapper.BookMapper;
 import com.example.onlinebookstore.model.Book;
 import com.example.onlinebookstore.repository.BookRepository;
 import com.example.onlinebookstore.service.BookService;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
-    @Autowired
     private BookRepository bookRepository;
+    private BookMapper bookMapper;
 
     @Override
-    public Book save(Book book) {
-        return bookRepository.save(book);
+    public BookDto save(CreateBookRequestDto requestDto) {
+        Book book = bookMapper.toModel(requestDto);
+        return bookMapper.toDto(bookRepository.save(book));
     }
 
     @Override
-    public List<Book> findAll() {
-        return bookRepository.findAll();
+    public List<BookDto> findAll() {
+        return bookRepository.findAll().stream()
+                .map(bookMapper::toDto)
+                .toList();
+    }
+
+    @Override
+    public BookDto getBookById(Long id) {
+        Book book = bookRepository.findBookById(id).orElseThrow(
+                () -> new EntityNotFoundException("Cannot find a book by id " + id));
+        return bookMapper.toDto(book);
     }
 }
