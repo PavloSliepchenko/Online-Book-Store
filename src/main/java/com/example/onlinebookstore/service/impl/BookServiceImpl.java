b45d8fb61b77d419c2e551f733bcfc0883b451c4
PavloSliepchenko <115042054+PavloSliepchenko@users.noreply.github.com>
2023-10-31 15:09:19 -0500

    Added JpaRepository to BookRepository, implemented soft delete, added… (#4)
    * Added JpaRepository to BookRepository, implemented soft delete, added missing end points to service layer
    
    * Revised update method implementation

--- src/main/java/com/example/onlinebookstore/service/impl/BookServiceImpl.java
+++ src/main/java/com/example/onlinebookstore/service/impl/BookServiceImpl.java
@@ -8,14 +8,19 @@ import com.example.onlinebookstore.model.Book;
 import com.example.onlinebookstore.repository.BookRepository;
 import com.example.onlinebookstore.service.BookService;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
     private BookRepository bookRepository;
     private BookMapper bookMapper;
+
+    @Autowired
+    public BookServiceImpl(BookRepository bookRepository, BookMapper bookMapper) {
+        this.bookRepository = bookRepository;
+        this.bookMapper = bookMapper;
+    }
 
     @Override
     public BookDto save(CreateBookRequestDto requestDto) {
@@ -31,9 +36,24 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
-    public BookDto getBookById(Long id) {
-        Book book = bookRepository.findBookById(id).orElseThrow(
+    public BookDto getById(Long id) {
+        Book book = bookRepository.findById(id).orElseThrow(
                 () -> new EntityNotFoundException("Cannot find a book by id " + id));
         return bookMapper.toDto(book);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        bookRepository.deleteById(id);
+    }
+
+    @Override
+    public BookDto update(Long id, CreateBookRequestDto bookDto) {
+        if (!bookRepository.existsById(id)) {
+            throw new EntityNotFoundException("No book was found with id " + id);
+        }
+        Book book = bookMapper.toModel(bookDto);
+        book.setId(id);
+        return bookMapper.toDto(bookRepository.save(book));
     }
 }
