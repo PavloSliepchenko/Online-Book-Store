18cb2494b28ac6357d2386793a88ee367e539c90
PavloSliepchenko <115042054+PavloSliepchenko@users.noreply.github.com>
2023-11-04 15:50:02 -0500

    Exception handler branch (#6)
    * Created and implemented DTOs, added Mapstruct and mappers, created BookController, added custom exception
    
    * Added JpaRepository to BookRepository, implemented soft delete, added missing end points to service layer
    
    * Revised update method implementation
    
    * Implemented ability to search books
    
    * Removed the ability to search by prices
    
    * Implemented validation for existing CreateBookRequestDto class, added GlobalExceptionHandler

--- src/main/java/com/example/onlinebookstore/dto/CreateBookRequestDto.java
+++ src/main/java/com/example/onlinebookstore/dto/CreateBookRequestDto.java
@@ -1,12 +1,18 @@
 package com.example.onlinebookstore.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
+    @NotNull
     private String title;
+    @NotNull
     private String isbn;
+    @NotNull
+    @Min(value = 0)
     private BigDecimal price;
     private String description;
     private String coverImage;
