60a97bc2b729c75b00ae29d13aa6fdce04289b94
PavloSliepchenko <115042054+PavloSliepchenko@users.noreply.github.com>
2023-11-25 10:55:57 -0600

    Added token authentication (#10)
    * Added token authentication
    
    * replaced @NotNull and @NotEmpty with @NotBlank

--- src/main/java/com/example/onlinebookstore/controller/AuthenticationController.java
+++ src/main/java/com/example/onlinebookstore/controller/AuthenticationController.java
@@ -1,7 +1,10 @@
 package com.example.onlinebookstore.controller;
 
 import com.example.onlinebookstore.dto.user.UserDto;
+import com.example.onlinebookstore.dto.user.UserLoginRequestDto;
+import com.example.onlinebookstore.dto.user.UserLoginResponseDto;
 import com.example.onlinebookstore.dto.user.UserRegistrationRequestDto;
+import com.example.onlinebookstore.security.AuthenticationService;
 import com.example.onlinebookstore.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -15,9 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api/auth")
 public class AuthenticationController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
 
     @PostMapping(value = "/registration")
     public UserDto registerUser(@RequestBody @Valid UserRegistrationRequestDto request) {
         return userService.save(request);
+    }
+
+    @PostMapping(value = "/login")
+    public UserLoginResponseDto login(@RequestBody @Valid UserLoginRequestDto request) {
+        return authenticationService.authenticate(request);
     }
 }
---
+++ src/main/java/com/example/onlinebookstore/dto/user/UserLoginRequestDto.java
@@ -0,0 +1,15 @@
+package com.example.onlinebookstore.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
+
+public record UserLoginRequestDto(
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        @Length(min = 5, max = 15)
+        String password
+) {
+}
