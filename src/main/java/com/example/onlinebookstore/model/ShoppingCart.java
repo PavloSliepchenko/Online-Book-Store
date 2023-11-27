025ea43e92c701ef178c6f5bf6db13330bb5b3a0
PavloSliepchenko <sliepchenkopavlo@gmail.com>
2023-11-27 07:37:49 -0600

    Added soft delete to shopping carts

--- src/main/java/com/example/onlinebookstore/model/ShoppingCart.java
+++ src/main/java/com/example/onlinebookstore/model/ShoppingCart.java
@@ -1,5 +1,6 @@
 package com.example.onlinebookstore.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -13,10 +14,14 @@ import java.util.Set;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Data
 @Entity
 @Table(name = "shopping_carts")
+@Where(clause = "is_deleted = false")
+@SQLDelete(sql = "UPDATE shopping_carts SET is_deleted = true WHERE id = ?")
 public class ShoppingCart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,4 +35,6 @@ public class ShoppingCart {
     @EqualsAndHashCode.Exclude
     @OneToMany(mappedBy = "shoppingCart", fetch = FetchType.EAGER)
     private Set<CartItem> cartItems;
+    @Column(nullable = false)
+    private boolean isDeleted = false;
 }
