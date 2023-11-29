910fec21f7bde86faa5be007f5043b8e963ff7c8
PavloSliepchenko <sliepchenkopavlo@gmail.com>
2023-11-29 05:54:58 -0600

    Changed relationship between OrderItem and Book

--- src/main/java/com/example/onlinebookstore/model/OrderItem.java
+++ src/main/java/com/example/onlinebookstore/model/OrderItem.java
@@ -7,7 +7,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import lombok.Data;
@@ -30,7 +29,7 @@ public class OrderItem {
     @EqualsAndHashCode.Exclude
     @JoinColumn(name = "order_id", nullable = false)
     private Order order;
-    @OneToOne
+    @ManyToOne
     @ToString.Exclude
     @EqualsAndHashCode.Exclude
     @JoinColumn(name = "book_id", nullable = false)
